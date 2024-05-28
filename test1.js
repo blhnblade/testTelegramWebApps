de5ce9472dfc6a8bb6d92d755fb4086a52f4d5fc
Artyom Torbokov <mail4242@bk.ru>
2024-05-28 14:52:02 +0700

    btn event

--- test1.js
+++ test1.js
@@ -1,11 +1,12 @@
+if (window.Telegram.WebApp) {
 const data = JSON.stringify({
     eventType: 'web_app_setup_back_button',
     eventData: {
       is_visible: true,
     },
-  });
-  
-  window.parent.postMessage(data, 'https://web.telegram.org');
+});
+
+window.parent.postMessage(data, 'https://web.telegram.org');
 
 
   const data1 = JSON.stringify({ is_visible: true });
@@ -13,3 +14,15 @@ const data = JSON.stringify({
 window
   .TelegramWebviewProxy
   .postEvent('web_app_setup_back_button', data1);
+
+    // Включаем кнопку "Назад"
+    Telegram.WebApp.BackButton.show();
+
+    // Обработчик события нажатия кнопки "Назад"
+    Telegram.WebApp.onEvent('backButtonPressed', function() {
+        console.log('Кнопка "Назад" нажата');
+        alert('Press')
+        // Ваша логика здесь
+        // Например, вы можете закрыть Web App или перейти на предыдущую страницу
+    });
+}
