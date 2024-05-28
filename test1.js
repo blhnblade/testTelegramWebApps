7ed1a88c54ee12a56a909fbbf998fee81dcd99c7
Artyom Torbokov <mail4242@bk.ru>
2024-05-28 15:21:58 +0700

    fix

--- test1.js
+++ test1.js
@@ -10,14 +10,7 @@ const data = JSON.stringify({
     },
 });
 
-window.parent.postMessage(data, 'https://web.telegram.org');
 
-
-  const data1 = JSON.stringify({ is_visible: true });
-
-window
-  .TelegramWebviewProxy
-  .postEvent('web_app_setup_back_button', data1);
 
     // // Включаем кнопку "Назад"
     // Telegram.WebApp.BackButton.show();
