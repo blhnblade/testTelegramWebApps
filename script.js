c6d5a5dc100461fb76c79512558e044a9fef4641
Artyom Torbokov <mail4242@bk.ru>
2024-05-28 14:20:14 +0700

    fix

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 console.log(123)
 console.log(window.Telegram.WebApp)
-document.body.appendChild = JSON.stringify(window.Telegram.WebApp)
+// document.body.appendChild = JSON.stringify(window.Telegram.WebApp)
