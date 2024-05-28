cc017b3bd63b59cd2c927c3fab7ba7f394b12e8a
Artyom Torbokov <mail4242@bk.ru>
2024-05-28 14:35:36 +0700

    btn back

---
+++ test1.js
@@ -0,0 +1,15 @@
+const data = JSON.stringify({
+    eventType: 'web_app_setup_back_button',
+    eventData: {
+      is_visible: true,
+    },
+  });
+  
+  window.parent.postMessage(data, 'https://web.telegram.org');
+
+
+  const data1 = JSON.stringify({ is_visible: true });
+
+window
+  .TelegramWebviewProxy
+  .postEvent('web_app_setup_back_button', data1);
